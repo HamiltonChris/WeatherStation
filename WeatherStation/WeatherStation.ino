4356317c7bf3a0e020eb6073fabc7697e4378739
HamiltonChris <chris.ross.hamilton@gmail.com>
2025-09-09 21:13:52 -0700

    Add tflite model to arduino code

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -1,9 +1,32 @@
+#include <TensorFlowLite.h>
+#include <TensorFlow/lite/micro/all_ops_resolver.h>
+#include <TensorFlow/lite/micro/micro_interpreter.h>
+#include <TensorFlow/lite/micro/micro_log.h>
+#include <TensorFlow/lite/micro/system_setup.h>
+#include <TensorFlow/lite/schema/schema_generated.h>
+
+#include "model2.h"
 #include "bme280.h"
 #include <Wire.h>
 
 void i2c_send(uint8_t address, uint8_t* data, uint8_t length);
 void i2c_receive(uint8_t address, uint8_t* data, uint8_t length);
 void debug_print(char* str);
+
+const tflite::Model *tflu_model = nullptr;
+tflite::MicroInterpreter *tflu_interpreter = nullptr;
+TfLiteTensor *tflu_i_tensor = nullptr;
+TfLiteTensor * tflu_o_tensor = nullptr;
+
+float tflu_i_scale = 1.0f;
+int32_t tflu_i_zero_point = 0;
+float tflu_o_scale = 1.0f;
+int32_t tflu_o_zero_point = 0;
+
+constexpr int t_sz = 4096;
+uint8_t tensor_arena[t_sz] __attribute__((aligned(16)));
+
+tflite::AllOpsResolver tflu_ops_resolver;
 
 bme280_t bme280;
 uint32_t pressure = 0;
@@ -16,9 +39,6 @@ constexpr int num_hours = 3;
 int8_t t_vals[num_hours] = {0};
 int8_t h_vals[num_hours] = {0};
 int cur_idx = 0;
-
-float tflu_i_scale = 1.0f;
-int32_t tflu_i_zero_point = 0;
 
 // Calculated Z-scores
 constexpr float t_mean = 2.63218;
@@ -55,7 +75,22 @@ void setup() {
   {
     Serial.print("Sensor Initialization complete\n");
   }
-  
+
+  tflu_model = tflite::GetModel(snow_model_tflite);
+  static tflite::MicroInterpreter static_interpreter(tflu_model, tflu_ops_resolver, tensor_arena, t_sz);
+  tflu_interpreter = &static_interpreter;
+
+  tflu_interpreter->AllocateTensors();
+  tflu_i_tensor = tflu_interpreter->input(0);
+  tflu_o_tensor = tflu_interpreter->output(0);
+
+  const auto* i_quant = reinterpret_cast<TfLiteAffineQuantization*>(tflu_i_tensor->quantization.params);
+  const auto* o_quant = reinterpret_cast<TfLiteAffineQuantization*>(tflu_o_tensor->quantization.params);
+
+  tflu_i_scale = i_quant->scale->data[0];
+  tflu_i_zero_point = i_quant->zero_point->data[0];
+  tflu_o_scale = o_quant->scale->data[0];
+  tflu_o_zero_point = o_quant->zero_point->data[0];
 }
 
 void loop() {
@@ -87,7 +122,7 @@ void loop() {
       t /= (float)num_reads;
       h /= (float)num_reads;
 
-      sprintf((char*)user_data, "Temp: %f  Humidity: %f", t, h);
+      sprintf((char*)user_data, "Temp: %f deg C Humidity: %f%%", t, h);
       Serial.println(user_data);
 
       t = (t - t_mean) / t_std;
@@ -102,6 +137,33 @@ void loop() {
       h_vals[cur_idx] = h;
 
       cur_idx = (cur_idx + 1) % num_hours;
+
+      // Prepare input features
+      int32_t idx0 = cur_idx;
+      int32_t idx1 = (cur_idx - 1 + num_hours) % num_hours;
+      int32_t idx2 = (cur_idx - 2 + num_hours) % num_hours;
+      tflu_i_tensor->data.int8[0] = t_vals[idx2];
+      tflu_i_tensor->data.int8[1] = t_vals[idx1];
+      tflu_i_tensor->data.int8[2] = t_vals[idx0];
+      tflu_i_tensor->data.int8[3] = h_vals[idx2];
+      tflu_i_tensor->data.int8[4] = h_vals[idx1];
+      tflu_i_tensor->data.int8[5] = h_vals[idx0];
+
+      tflu_interpreter->Invoke();
+
+      float out_int8 = tflu_o_tensor->data.int8[0];
+      float out_f = (out_int8 - tflu_o_zero_point);
+      out_f *= tflu_o_scale;
+
+      if (out_f > 0.5)
+      {
+        Serial.println("Yes, it snows");
+        Serial.println(out_f);
+      }
+      else
+      {
+        Serial.println("No, it doesn't snow");
+      }
 
       delay(1000); // should be hours in production
     }
